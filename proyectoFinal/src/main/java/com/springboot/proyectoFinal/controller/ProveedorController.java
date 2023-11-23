4a735ebe72482b86cb705502df7239ea7b91d497
David Ramos <dr264289@gmail.com>
2023-11-23 10:56:22 -0600

    Added method for update in proveedor

--- proyectoFinal/src/main/java/com/springboot/proyectoFinal/controller/ProveedorController.java
+++ proyectoFinal/src/main/java/com/springboot/proyectoFinal/controller/ProveedorController.java
@@ -1,12 +1,23 @@
 package com.springboot.proyectoFinal.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.introspect.AccessorNamingStrategy.Provider;
 import com.google.gson.Gson;
+import com.springboot.proyectoFinal.models.entity.Proveedor;
 import com.springboot.proyectoFinal.repositorios.ProveedorRepository;
+
+
 
 @RestController
 @RequestMapping("/proveedor")
@@ -22,6 +33,37 @@ public class ProveedorController {
 		return json.toJson(proveedorDAO.findAll());
 	}
 	
+	@PostMapping(value = "/nuevoProveedor", produces = "application/json")
+	public Proveedor nuevoProveedor(@RequestParam("proveedor") String proveedor , @RequestParam("direccion") String direccion)
+	{
+		Proveedor prov = new Proveedor();
+		
+		prov.setNombre(proveedor);
+		prov.setDireccion(direccion);
+		
+		return proveedorDAO.save(prov);
+		
+	}
 	
+	@GetMapping(value = "/porId", produces = "application/json")
+	public Optional<Proveedor> ProveedorPorId(@RequestParam("id") Long id)
+	{
+		
+		return proveedorDAO.findById(id);
+		
+	}
+	
+	@PutMapping(value="/actualizar", produces = "application/json")
+	public Proveedor actualizarProveedor(@RequestParam("proveedor") String nombre, @RequestParam("direccion") String direccion, @RequestParam("id") Long id)
+	{
+		
+		Optional<Proveedor>  provider = proveedorDAO.findById(id);
+		Proveedor prov = provider.get();
+		prov.setNombre(nombre);
+		prov.setDireccion(direccion);
+		return proveedorDAO.save(prov);
+		
+		
+	}
 
 }
